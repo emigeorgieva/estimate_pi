a270ae7f521003187572553b17f814d74ad3109f
Emilia Chukaleva <georgievaemillia@gmail.com>
2023-08-22 17:18:19 +0300

    random.uniform function to get x and y numbers to get points coordinates (x,y), calculate distance of a point, determining number of points in the circle and in the whole square, calculating pi

--- og_problem/main.py
+++ og_problem/main.py
@@ -1,4 +1,20 @@
 import random
 
+
 def estimate_pi(n):
-    num_points_circle, num_points_square = 0
+    num_points_circle = 0
+    num_points_square = 0
+
+    for i in range(n):
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
+        distance_of_point = x ** 2 + y ** 2
+        if distance_of_point < 1:
+            num_points_circle += 1
+        num_points_square += 1
+
+    pi = 4 * (num_points_circle / num_points_square)
+    return pi
+
+
+print(estimate_pi(1000))
